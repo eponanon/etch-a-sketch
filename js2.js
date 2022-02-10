e6fed3a97113c9519289ed46835c768d31e72b8b
eponanon <89871168+eponanon@users.noreply.github.com>
2022-02-10 11:52:21 -0500

    Everything Works. No rainbow effect.

--- js2.js
+++ js2.js
@@ -2,31 +2,50 @@ const container = document.getElementById("sketchpad");
 const square = document.createElement("div");
 const squaresList = container.childNodes;
 
-function createSquares() {
-    for (let i = 0; i < 256; i++) {
+const scottQuote = document.getElementById("heading2");
+scottQuote.textContent = "'Behold the Power of Art' ~ Michael Scott";
+
+function createSquares(num) {
+    for (let i = 0; i < num; i++) {
         square.className = " gridSquare";
         container.append(square.cloneNode(true));
     }
 }
 
-createSquares();
-
-function changeColor() {
-    this.style.backgroundColor = "yellow";
-}
-
-for (let i = 0; i < squaresList.length; i++) {
-    squaresList[i].addEventListener("mouseover", changeColor);
-}
-
-const scottQuote = document.getElementById("heading2");
-scottQuote.textContent = "'Behold the Power of Fart' ~ Michael Scott";
-
-function resetGrid() {
-    for (let i = 0; i < squaresList.length; i++) {
-        squaresList[i].style.backgroundColor = "gray";
+function createSmallSquares(num) {
+    for (let i = 0; i < num; i++) {
+        square.className = " gridSquareSmall";
+        container.append(square.cloneNode(true));
     }
 }
 
-const resetBtn = document.getElementById("resetBtn");
-resetBtn.addEventListener('click', () => {resetGrid();});
+function changeColor() {
+    this.style.backgroundColor = "black";
+}
+
+function addMouseOver() {
+    for (let i = 0; i < squaresList.length; i++) {
+        squaresList[i].addEventListener("mouseover", changeColor);
+    }
+}
+
+//reset1 draws and resets a grid with big squares
+function reset1(num) {
+    container.textContent = "";
+    createSquares(num);
+    addMouseOver();
+}
+reset1(256);
+
+//reset2 draws and resets a grid with finer squares
+function reset2(num) {
+    container.textContent = "";
+    createSmallSquares(num);
+    addMouseOver();
+}
+
+const resetBtn1 = document.getElementById("resetBtn1");
+resetBtn1.addEventListener('click', () => {reset1(256);});
+
+const resetBtn2 = document.getElementById("resetBtn2");
+resetBtn2.addEventListener('click', () => {reset2(625);});
